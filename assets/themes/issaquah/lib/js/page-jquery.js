cf4c649d37e0d62d5c473005d0fc4ef4d2c7973d
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2016-03-28 22:54:47 -0400

    inner page nav and trying to fix the equal column issue with it overridging the content

--- assets/themes/issaquah/lib/js/page-jquery.js
+++ assets/themes/issaquah/lib/js/page-jquery.js
@@ -1,6 +1,8 @@
 jQuery(document).ready(function($) {    
-    $(".sidebar-content .content-sidebar-wrap main.content,.sidebar-content .content-sidebar-wrap aside.sidebar-primary").equalHeightColumns();
-
+    $(".sidebar-content .content-sidebar-wrap main.content,.sidebar-content .content-sidebar-wrap aside.sidebar-primary").equalHeightColumns(
+        {checkHeight: 'innerHeight'}
+    );
+    
     //do some little stuff for parralaxing
     // init controller
     var controller = new ScrollMagic({globalSceneOptions: {triggerHook: "onEnter", duration: $(window).height()*2}});
@@ -10,8 +12,8 @@ jQuery(document).ready(function($) {
         .setTween(TweenMax.fromTo("#page-title-area .banner", 1, {css:{'background-position':"50% 40%"}, ease: Linear.easeNone}, {css:{'background-position':"50% -40%"}, ease: Linear.easeNone}))
         .addTo(controller);
     //do some nifty stuff for the menu
-    $('.widget_advanced_menu .menu>li.current-menu-item,.widget_advanced_menu .menu>li.current-menu-ancestor').addClass('open');
-    $('.widget_advanced_menu .menu>li').prepend(function(){
+    $('.widget_advanced_menu .menu>li>.sub-menu>li.current-menu-item,.widget_advanced_menu .menu>li>.sub-menu>li.current-menu-ancestor').addClass('open');
+    $('.widget_advanced_menu .menu>li>.sub-menu>li').prepend(function(){
         if($(this).hasClass('menu-item-has-children')){
             if($(this).hasClass('open')){
                 return '<i class="fa fa-minus"></i>';
@@ -19,21 +21,29 @@ jQuery(document).ready(function($) {
                 return '<i class="fa fa-plus"></i>';
             }
         } else {
-            return '';
+            return '<i class="fa"></i>';
         }
     });
-    $('.widget_advanced_menu .menu>li>i').click(function(){
-        var old = $('.widget_advanced_menu .menu>li.open');
+    $('.widget_advanced_menu .menu>li>.sub-menu>li>i.fa').click(function(){
+        var old = $('.widget_advanced_menu .menu>li>.sub-menu>li.open');
         var cur = $(this).parent();
-        old.removeClass('open').find('i').removeClass('fa-minus').addClass(function(){
-            if($(this).parent().hasClass('menu-item-has-children')){
-                return 'fa-plus';
-            }
-        });
-        cur.addClass('open').find('i').removeClass('fa-plus').addClass(function(){
-            if($(this).parent().hasClass('menu-item-has-children')){
-                return 'fa-minus';
-            }
-        });
+        if(cur.hasClass('open')){
+            cur.removeClass('open').find('i').removeClass('fa-plus').addClass(function(){
+                if($(this).parent().hasClass('menu-item-has-children')){
+                    return 'fa-minus';
+                }
+            });
+        } else {
+            old.removeClass('open').find('i').removeClass('fa-minus').addClass(function(){
+                if($(this).parent().hasClass('menu-item-has-children')){
+                    return 'fa-plus';
+                }
+            });
+            cur.addClass('open').find('i').removeClass('fa-plus').addClass(function(){
+                if($(this).parent().hasClass('menu-item-has-children')){
+                    return 'fa-minus';
+                }
+            });
+        }
     });
 });
